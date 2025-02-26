e673bad5e3a939f70a29b05ea78ff8e5374efe55
deonas <deonasaji@gmail.com>
2025-02-26 16:10:59 +0530

    Updated

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["media3.giphy.com"], // Add the domain here
+    },
+  };
+  
+  module.exports = nextConfig;
